2b13e3dcae4ffdbcb3f03ac59170fdec97f1ce49
ReyhanHerdi <reyhanherdiyanto180603@students.unnes.ac.id>
2024-01-16 19:22:00 +0700

    Set view

---
+++ RicknMorty/app/src/main/java/com/example/ricknmorty/HomeActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ricknmorty
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.ricknmorty.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+
+//        val appBarConfiguration = AppBarConfiguration.Builder(
+//            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//        ).build()
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
